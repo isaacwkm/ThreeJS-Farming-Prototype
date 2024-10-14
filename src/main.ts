f492c07dad596a52083c1ba9f536473fa6124fb5
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-10-13 20:00:37 -0700

    Step 1 complete - Added canvas with CSS styling

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,8 @@ document.title = appName;
 const header = document.createElement("h1");
 header.innerHTML = appName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.setAttribute("height", "256px");
+canvas.setAttribute("width", "256px");
+app.append(canvas);
