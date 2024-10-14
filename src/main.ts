6a1d4150121c02538b36e6a07b0aec457aa5f7bc
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2024-10-14 15:31:20 -0700

    Added rendering context and mouse events

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,17 @@ const canvas = document.createElement("canvas");
 canvas.setAttribute("height", "256px");
 canvas.setAttribute("width", "256px");
 app.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+canvas.addEventListener("mousedown", (e) => {
+    ctx?.beginPath();
+    ctx?.moveTo(e.offsetX, e.offsetY);
+})
+canvas.addEventListener("mousemove", (e) => {
+    ctx?.lineTo(e.offsetX, e.offsetY);
+})
+canvas.addEventListener("mouseup", (e) => {
+    ctx?.lineTo(e.offsetX, e.offsetY);
+    ctx?.stroke();
+})
