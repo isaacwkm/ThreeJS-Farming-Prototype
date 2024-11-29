d4913c1918fc5ce65483c2befde4f2e3781eff5d
AlphaOwl <mrmundychannel@gmail.com>
2024-11-29 01:31:12 -0800

    updated to current version of ts github 11/29/2024

--- src/main.js
+++ src/main.js
@@ -80,10 +80,11 @@ function animate() {
 
 animate();
 
-/*
 import { Grid } from "./models.ts";
 import { Player } from "./models.ts";
+import { Plant } from "./models.ts";
 
+/*
 interface Command {
     execute(): void;
     undo(): void;
@@ -92,18 +93,23 @@ interface Command {
 interface Save {
     playerPos: { x: number, y: number };
     gridState: string;
+    plantMap: [string, Plant][];
+    inGameTime: number;
     timestamp: string;
 }
 
-localStorage.clear();
-
 const grid = new Grid();
-const playerCharacter = new Player(0, 0, grid.GRID_WIDTH);
+const playerCharacter = new Player(0, 0, grid.GRID_WIDTH, grid.GRID_WIDTH);
 
 const undoStack: Command[] = [];
 const redoStack: Command[] = [];
 
-function createMoveCommand(player: Player, dx: number, dy: number) : Command | null {
+const plants = new Map<string, Plant>();
+let currentPlantType = "🌽";
+let currentDay = 0;
+let reapFull = 0;
+
+function createMoveCommand(player: Player, dx: number, dy: number): Command | null {
     const data = { before_dx: 0, before_dy: 0 };
     if (player.boundsCheck(dx, dy)) {
         return {
@@ -120,8 +126,12 @@ function createMoveCommand(player: Player, dx: number, dy: number) : Command | n
     return null;
 }
 
-function createTurnCommand(grid: Grid) : Command {
-    const data = { before_grid: grid.serialize(), after_grid: "" }
+function createTurnCommand(grid: Grid): Command {
+    const data = {
+        before_grid: grid.serialize(),
+        after_grid: "",
+        growthMap: new Map<string, number>()
+    }
     return {
         execute() {
             if (!data.after_grid) {
@@ -130,16 +140,63 @@ function createTurnCommand(grid: Grid) : Command {
             } else {
                 grid.deserialize(data.after_grid);
             }
+            for (const [key, plant] of plants) {
+                data.growthMap.set(key, plant.growthStage);
+                plant.grow(grid.getSunAt(plant.x, plant.y), grid.getWaterAt(plant.x, plant.y), plants);
+            }
         },
         undo() {
             grid.deserialize(data.before_grid);
+            for (const [key, plant] of plants) {
+                plant.growthStage = data.growthMap.get(key)!;
+            }
         }
     }
 }
 
-function handleInput(key: string) {
+function createSowCommand(x: number, y: number): Command {
+    const data = { plant: Plant.switchPlant(currentPlantType, x, y, 0) }
+    return {
+        execute() {
+            plants.set(`${x}${y}`, data.plant);
+            grid.sowCell(x, y);
+        },
+        undo() {
+            plants.delete(`${x}${y}`);
+            grid.sowCell(x, y);
+        }
+    }
+}
+
+function createReapCommand(x: number, y: number): Command {
+    const data = { plant: plants.get(`${x}${y}`)! }
+    return {
+        execute() {
+            plants.delete(`${x}${y}`);
+            grid.sowCell(x, y);
+            advanceScenario(data.plant);
+        },
+        undo() {
+            plants.set(`${x}${y}`, data.plant);
+            grid.sowCell(x, y);
+            revertScenario(data.plant);
+        }
+    }
+}
+
+function advanceScenario(plant: Plant) {
+    if (plant.growthStage == 3)
+        reapFull++;
+}
+
+function revertScenario(plant: Plant) {
+    if (plant.growthStage == 3)
+        reapFull--;
+}
+
+function handleKeyboardInput(key: string) {
     redoStack.splice(0, redoStack.length);
-    
+
     const inputMap: Record<string, Command> = {
         "ArrowLeft": createMoveCommand(playerCharacter, -1, 0)!,
         "ArrowRight": createMoveCommand(playerCharacter, 1, 0)!,
@@ -149,6 +206,21 @@ function handleInput(key: string) {
     };
 
     const command = inputMap[key];
+    manageCommand(command);
+}
+
+function farmTheLand(x: number, y: number) {
+    redoStack.splice(0, redoStack.length);
+
+    if (playerCharacter.isAdjacent(x, y)) {
+        if (!grid.readCell(x, y).sowed)
+            manageCommand(createSowCommand(x, y));
+        else
+            manageCommand(createReapCommand(x, y));
+    }
+}
+
+function manageCommand(command: Command) {
     if (command) {
         undoStack.push(command);
         command.execute();
@@ -178,12 +250,33 @@ function createSave(key: string) {
     const saveFile: Save = {
         playerPos: { x: playerCharacter.x, y: playerCharacter.y },
         gridState: grid.serialize(),
+        plantMap: Array.from(plants.entries()),
+        inGameTime: currentDay,
         timestamp: new Date().toISOString(),
     };
 
     const saveData = JSON.stringify(saveFile);
     localStorage.setItem(key, saveData);
-    console.log(`Game saved under ${key}:`, saveFile);
+    if (key != "autosave")
+        console.log(`Game saved under ${key}`);
+}
+
+function copyDataFromFile(saveFile: Save) {
+    playerCharacter.x = saveFile.playerPos.x;
+    playerCharacter.y = saveFile.playerPos.y;
+    grid.deserialize(saveFile.gridState);
+
+    plants.clear();
+    saveFile.plantMap.forEach((plant: [string, Plant]) => {
+        plants.set(plant[0], Plant.deepCopy(plant[1]));
+    });
+}
+
+function listSaves() {
+    console.log("Saves found:");
+    for (let i = 0, len = localStorage.length; i < len; i++) {
+        console.log(localStorage.key(i)!);
+    }
 }
 
 function loadSave(key: string) {
@@ -197,11 +290,24 @@ function loadSave(key: string) {
     undoStack.splice(0, undoStack.length);
     redoStack.splice(0, redoStack.length);
 
-    playerCharacter.x = saveFile.playerPos.x;
-    playerCharacter.y = saveFile.playerPos.y;
-    grid.deserialize(saveFile.gridState);
+    copyDataFromFile(saveFile);
+
     notify("scene-changed");
     console.log(`Game loaded from save ${key}`);
+}
+
+function autosavePrompt() {
+    if (localStorage.getItem("autosave")) {
+        if (confirm("Would you like to continue where you left off?"))
+            loadSave("autosave");
+        else
+            localStorage.removeItem("autosave");
+    }
+}
+
+function checkScenarioWin() {
+    if (reapFull >= 20)
+        return true;
 }
 
 type EventName = "scene-changed";
@@ -210,7 +316,7 @@ function notify(name: EventName) {
 }
 
 window.addEventListener("keydown", (e) => {
-    handleInput(e.key);
+    handleKeyboardInput(e.key);
 })
 
 const canvas = document.createElement("canvas");
@@ -220,17 +326,71 @@ document.body.appendChild(canvas);
 const ctx = canvas.getContext("2d")!;
 const tileWidth = canvas.width / grid.GRID_WIDTH;
 
-function drawPlayer(player: Player) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+canvas.addEventListener("click", (e) => {
+    const mouseX = e.offsetX;
+    const mouseY = e.offsetY;
 
+    const gridX = Math.floor(mouseX / tileWidth);
+    const gridY = Math.floor(mouseY / tileWidth);
+
+    farmTheLand(gridX, gridY);
+})
+
+function drawGrid() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "black";
+    for (let x = 0; x <= grid.GRID_WIDTH; x++) {
+        ctx.beginPath();
+        ctx.moveTo(x * tileWidth, 0);
+        ctx.lineTo(x * tileWidth, canvas.height);
+        ctx.stroke();
+    }
+    for (let y = 0; y <= grid.GRID_WIDTH; y++) {
+        ctx.beginPath();
+        ctx.moveTo(0, y * tileWidth);
+        ctx.lineTo(canvas.width, y * tileWidth);
+        ctx.stroke();
+    }
+}
+
+function drawPlayer(player: Player) {
     const basePositionX = tileWidth * player.x;
     const basePositionY = tileWidth * player.y;
     const centerOffset = tileWidth / 2 - 10;
+
+    ctx.fillStyle = "black";
     ctx.fillRect(basePositionX + centerOffset, basePositionY + centerOffset, 20, 20);
 }
 
+function drawPlants() {
+    for (const [key, plant] of plants) {
+        const basePositionX = tileWidth * plant.x;
+        const basePositionY = tileWidth * plant.y;
+        const centerOffset = tileWidth / 4;
+        const fontList = [
+            "24px monospace",
+            "28px monospace",
+            "32px monospace",
+            "36px monospace"
+        ]
+        ctx.font = fontList[plant.growthStage];
+        ctx.fillText(plant.type, basePositionX + centerOffset, basePositionY + 2.5 * centerOffset);
+    }
+}
+
 canvas.addEventListener("scene-changed", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawGrid();
     drawPlayer(playerCharacter);
+    drawPlants();
+    createSave("autosave");
+
+    const winnerNode = document.getElementById("winner");
+    if (winnerNode)
+        winnerNode.remove();
+    if (checkScenarioWin())
+        document.body.appendChild(winText);
 })
 
 const undoButton = document.createElement("button");
@@ -254,10 +414,29 @@ document.body.appendChild(saveButton);
 const loadButton = document.createElement("button");
 loadButton.innerHTML = "Load";
 loadButton.addEventListener("click", () => {
+    listSaves();
     const key = prompt("Enter save name")!;
     loadSave(key);
 })
 document.body.appendChild(loadButton);
 
+function createPlantButton(icon: string) {
+    const plantButton = document.createElement("button");
+    plantButton.innerHTML = `${icon}`;
+    plantButton.addEventListener("click", () => {
+        currentPlantType = icon;
+        console.log(currentPlantType);
+    })
+    return plantButton;
+}
+
+const winText = document.createElement("h1");
+winText.innerHTML = "You win!"
+winText.id = "winner";
+
+document.body.appendChild(createPlantButton("🌽"));
+document.body.appendChild(createPlantButton("🫘"));
+
+autosavePrompt();
 notify("scene-changed");
 */
--- src/models.ts
+++ src/models.ts
@@ -1,3 +1,12 @@
+/*
+interface Cell {
+    i: number;
+    j: number;
+    sun: number;
+    water: number;
+    sowed: number;
+}
+
 export class Grid {
     public readonly GRID_WIDTH = 6;
 
@@ -5,7 +14,8 @@ export class Grid {
     private readonly rowOffset = 4;
     private readonly sunOffset = 8;
     private readonly waterOffset = 12;
-    private readonly cellSize = 16;
+    private readonly sowOffset = 16;
+    private readonly cellSize = 20;
 
     private readonly numCells = this.GRID_WIDTH * this.GRID_WIDTH;
     private grid = new ArrayBuffer(this.cellSize * this.numCells);
@@ -16,7 +26,7 @@ export class Grid {
             for (let row = 0; row < this.GRID_WIDTH; row++) {
                 const sun = Math.floor(Math.random() * 10);
                 const water = Math.floor(Math.random() * 3);
-                this.writeCell(col, row, sun, water);
+                this.initCell(col, row, sun, water);
             }
         }
     }
@@ -27,24 +37,45 @@ export class Grid {
         return cellOffset;
     }
 
-    writeCell(i: number, j: number, sun: number, water: number) {
+    initCell(i: number, j: number, sun: number, water: number) {
         const cellOffset = this.getCellOffset(i, j);
 
         this.gridView.setInt32(cellOffset + this.colOffset, i);
         this.gridView.setInt32(cellOffset + this.rowOffset, j);
         this.gridView.setInt32(cellOffset + this.sunOffset, sun);
         this.gridView.setInt32(cellOffset + this.waterOffset, water);
+        this.gridView.setInt8(cellOffset + this.sowOffset, 0);
     }
 
-    readCell(col: number, row: number) {
+    readCell(col: number, row: number): Cell {
         const cellOffset = this.getCellOffset(col, row);
 
         const i = this.gridView.getInt32(cellOffset + this.colOffset);
         const j = this.gridView.getInt32(cellOffset + this.rowOffset);
         const sun = this.gridView.getInt32(cellOffset + this.sunOffset);
         const water = this.gridView.getInt32(cellOffset + this.waterOffset);
+        const sowed = this.gridView.getInt32(cellOffset + this.sowOffset);
 
-        return { i, j, sun, water };
+        return { i, j, sun, water, sowed};
+    }
+
+    getSunAt(col: number, row: number) {
+        const cellOffset = this.getCellOffset(col, row);
+        return this.gridView.getInt32(cellOffset + this.sunOffset);
+    }
+
+    getWaterAt(col: number, row: number) {
+        const cellOffset = this.getCellOffset(col, row);
+        return this.gridView.getInt32(cellOffset + this.waterOffset);
+    }
+
+    sowCell(col: number, row: number) {
+        const cellOffset = this.getCellOffset(col, row);
+        const sowBool = this.gridView.getInt32(cellOffset + this.sowOffset);
+        if (sowBool == 0)
+            this.gridView.setInt32(cellOffset + this.sowOffset, 1);
+        else
+            this.gridView.setInt32(cellOffset + this.sowOffset, 0);
     }
 
     randomize() {
@@ -54,7 +85,7 @@ export class Grid {
                 const sun = Math.floor(Math.random() * 10);
                 this.gridView.setInt32(cellOffset + this.sunOffset, sun);
 
-                const waterVars : number[] = [-1, -1, 0, 1, 1];
+                const waterVars: number[] = [-1, 0, 1, 2];
                 const waterDelta = waterVars[Math.floor(Math.random() * waterVars.length)];
                 let water = this.gridView.getInt32(cellOffset + this.waterOffset) + waterDelta;
                 water = Math.min(Math.max(water, 0), 3);
@@ -80,7 +111,7 @@ export class Grid {
 }
 
 export class Player {
-    constructor(public x: number, public y: number, private upperBound: number) {}
+    constructor(public x: number, public y: number, private xMax: number, private yMax: number) { }
 
     move(dx: number, dy: number) {
         this.x += dx;
@@ -90,14 +121,118 @@ export class Player {
     boundsCheck(dx: number, dy: number) {
         const x = this.x + dx;
         const y = this.y + dy;
-        if (x < 0 || this.upperBound <= x)
+        if (x < 0 || this.xMax <= x)
             return false;
-        if (y < 0 || this.upperBound <= y)
+        if (y < 0 || this.yMax <= y)
             return false;
         return true;
+    }
+
+    isAdjacent(gridX: number, gridY: number) {
+        const adjColumn = this.x - 1 <= gridX && gridX <= this.x + 1;
+        const adjRow = this.y - 1 <= gridY && gridY <= this.y + 1;
+
+        if (adjColumn && adjRow)
+            return true;
     }
 
     getPosition() {
         return { x: this.x, y: this.y };
     }
 }
+
+export class Plant {
+    protected familyNeighbors: number = 0; // Keeps track of same plants in neighboring cells
+
+    constructor(
+        public type: string,  // Example: "flower", "tree", etc.
+        public x: number,
+        public y: number,
+        public growthStage: number = 0,  // Tracks how grown a plant is
+        public minSun: number = 1,
+        public minWater: number = 1,
+    ) {}
+
+    grow(sun: number, water: number, plantMap: Map<string, Plant>) {}
+
+    protected checkSunAndWater(sun: number, water: number): boolean {
+        // Check if the passed values meet minimum conditions
+        if (sun < this.minSun) {
+            // needs more sun
+            return false;
+        }
+        if (water < this.minWater) {
+            // needs more water
+            return false;
+        }
+        return true; // Meets conditions
+    }
+
+    protected checkNeighborsForBoost(x: number, y: number, plantMap: Map<string, Plant>) {
+        this.familyNeighbors = 0;
+        for (let dx = -1; dx <= 1; dx++) {
+            for (let dy = -1; dy <= 1; dy++) {
+                if (dx === 0 && dy === 0) continue; // Skip self
+                const neighborX = x + dx;
+                const neighborY = y + dy;
+
+                const neighborKey = neighborX.toString() + neighborY.toString();
+                const neighborPlant = plantMap.get(neighborKey);
+                if (neighborPlant && (neighborPlant.type === this.type)) {
+                    this.familyNeighbors++;
+                }
+            }
+        }
+    }
+
+    static switchPlant(type: string, x: number, y: number, growthStage: number) {
+        switch(type) {
+            case "🫘": return new BeanPlant(x, y, growthStage);
+            case "🌽": return new CornPlant(x, y, growthStage);
+            default: throw new Error("Plant unrecognized");
+        }
+    }
+
+    static deepCopy(plant: Plant) {
+        const plantCopy = this.switchPlant(plant.type, plant.x, plant.y, plant.growthStage);
+        return plantCopy;
+    }
+}
+
+export class BeanPlant extends Plant {
+    constructor(
+        x: number,
+        y: number,
+        growthStage: number = 0,
+    ) {
+        super("🫘", x, y, growthStage, 3, 2); // Beans need minimum 3 sun, 2 water
+    }
+
+    // grow method: Beans grow faster with neighbors
+    override grow(sun: number, water: number, plantMap: Map<string, Plant>) {
+        this.checkNeighborsForBoost(this.x, this.y, plantMap); // this.familyNeighbors is ready to use
+        const canGrow = this.checkSunAndWater(sun, water) && this.growthStage < 3;
+        if (canGrow && this.familyNeighbors >= 2){
+            this.growthStage++;
+        }
+    }
+}
+
+export class CornPlant extends Plant {
+    constructor(
+        x: number,
+        y: number,
+        growthStage: number = 0,
+    ) {
+        super("🌽", x, y, growthStage, 1, 2);
+    }
+
+    override grow(sun: number, water: number) {
+        // Logic for plant growth
+        const canGrow = this.checkSunAndWater(sun, water) && this.growthStage < 3;
+        if (canGrow) {
+            this.growthStage++;
+        }
+    }
+}
+*/
